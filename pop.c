1751ba1cd174c0d7fb13b422a8315c33fa4b15bd
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2023-12-16 19:27:39 -0800

    update

--- pop.c
+++ pop.c
@@ -7,7 +7,7 @@
 */
 void f_pop(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
+	stack_t *hd;
 
 	if (*head == NULL)
 	{
@@ -17,7 +17,7 @@ void f_pop(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
-	*head = h->next;
-	free(h);
+	hd = *head;
+	*head = hd->next;
+	free(hd);
 }
